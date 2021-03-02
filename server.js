31afe9fa4cd5b3f10baf12b8d42e701bc82cb23f
Nagesh Mishra <nagesh3.13@gmail.com>
2021-03-02 23:42:46 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const favicon = require('express-favicon');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const path = require('path');
+const port = process.env.PORT || 8080;
+const app = express();
+app.use(favicon(__dirname + '/build/favicon.ico'));
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+app.use('/campaign', createProxyMiddleware({ target: 'https://dialitnow.ga:8080', changeOrigin: true }));
+
+
+app.listen(port);
